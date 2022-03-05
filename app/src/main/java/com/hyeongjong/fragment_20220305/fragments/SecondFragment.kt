78257525f18c5a442e724c412ea43e2eb492cd8a
이형종 <hj36879@naver.com>
2022-03-05 13:58:23 +0900

    프래그먼트에서 토스트 띄우기  - requireContext() 함수 활용

--- app/src/main/java/com/hyeongjong/fragment_20220305/fragments/SecondFragment.kt
+++ app/src/main/java/com/hyeongjong/fragment_20220305/fragments/SecondFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.hyeongjong.fragment_20220305.R
+import kotlinx.android.synthetic.main.fragment_second.*
 
 class SecondFragment : Fragment() {
 
@@ -21,6 +22,10 @@ class SecondFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        btnToastInFragment.setOnClickListener {
+            Toast.makeText(requireContext(), "토스트 띄우기", Toast.LENGTH_SHORT).show()
+
+        }
 
     }
 
